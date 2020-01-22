521eba94d432183d08ad50b6a07b5c2376493ad6
Tony ermakov <demimos.ll@gmail.com>
2020-01-22 18:46:23 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace AnalogClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dateText = new System.Windows.Forms.Label();
+            this.clockBox = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.clockBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 428);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // dateText
+            // 
+            this.dateText.AutoSize = true;
+            this.dateText.Location = new System.Drawing.Point(12, 263);
+            this.dateText.Name = "dateText";
+            this.dateText.Size = new System.Drawing.Size(0, 13);
+            this.dateText.TabIndex = 1;
+            // 
+            // clockBox
+            // 
+            this.clockBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.clockBox.Location = new System.Drawing.Point(12, 12);
+            this.clockBox.Name = "clockBox";
+            this.clockBox.Size = new System.Drawing.Size(410, 410);
+            this.clockBox.TabIndex = 2;
+            this.clockBox.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 459);
+            this.Controls.Add(this.clockBox);
+            this.Controls.Add(this.dateText);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.clockBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label dateText;
+        private System.Windows.Forms.PictureBox clockBox;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnalogClock
+{
+   
+    public partial class Form1 : Form
+    {
+        int Center = 205;
+        int radiusNum=190;
+        public Form1()
+        {
+            InitializeComponent();
+  
+        }
+
+        
+        
+
+
+        private void UpdateTime()
+        {
+            if (clockBox.BackgroundImage==null)
+            {
+                BackGroundRender();
+            }
+            var d = DateTime.Now;
+            var t = clockBox.CreateGraphics();
+
+            //Рисуем стрелки
+            double lengthSeconds = radiusNum - 10;
+            double lengthMinutes = radiusNum - 15;
+            double lengthHour = lengthMinutes / 1.5;
+           
+            double t_sec =  d.Second*6;                           //Определяем угол для секунд
+            double t_min = ((double)d.Minute + (double)d.Second/60)*6; //Определяем угол для минут
+            double t_hour =  ((double)d.Hour +  (double)d.Minute/60)*30; //Определяем угол для часов
+
+            //Рисуем часы
+            t.DrawLine(new Pen(Color.DarkGray, 7), Center, Center, (float)(Center + lengthHour * Math.Cos(Math.PI / 2 - t_hour * (Math.PI / 180))),
+                (float)(Center - lengthHour * Math.Sin(Math.PI / 2 - t_hour * (Math.PI / 180))));
+
+            //Рисуем минуты
+            t.DrawLine(new Pen(Color.Black, 5), Center, Center, (float)(Center + lengthMinutes * Math.Cos(Math.PI / 2 - t_min * (Math.PI / 180))),
+                (float)(Center - lengthMinutes * Math.Sin(Math.PI / 2 - t_min * (Math.PI / 180))));
+
+            //Рисуем секунды
+            t.DrawLine(new Pen(Color.Red), Center, Center, (float)(Center + lengthSeconds * Math.Cos(Math.PI / 2 - t_sec * (Math.PI / 180))),
+                (float)(Center - lengthSeconds * Math.Sin(Math.PI / 2 - t_sec * (Math.PI / 180))));
+
+
+        }
+
+        private void BackGroundRender()
+        {
+            var back = new Bitmap(410, 410);
+            var g = Graphics.FromImage(back);
+            g.Clear(Color.White);
+            g.DrawEllipse(new Pen(Color.Black, 2) { Width = 2.0f }, new Rectangle(5, 5, 400, 400));
+            radiusNum = 190;
+            for (var tm = 0; tm < 60; tm++)
+            {
+                int width;
+                if (tm % 5 == 0)
+                    width = 5;
+                else
+                    width = 2;
+                var xPointM = Center + radiusNum * Math.Cos(-6 * tm * (Math.PI / 180) + Math.PI / 2);
+                var yPointM = Center - radiusNum * Math.Sin(-6 * tm * (Math.PI / 180) + Math.PI / 2);
+                g.DrawEllipse(new Pen(Color.Black, 1), new Rectangle((int)(xPointM - width), (int)(yPointM - width), width * 2, width * 2));
+
+            }
+
+            //Оцифровка циферблата часов
+            var start = 1;
+            var stop = 12;
+            //циферблат 1-12 или 13-24
+            //if (date[3] >= 12)
+            //{
+            //    start = 13;
+            //    stop = 24;
+            //}
+            for (var th = start; th <= stop; th++)
+            {
+                var xText = Center + (radiusNum - 20) * Math.Cos(-30 * th * (Math.PI / 180) + Math.PI / 2);
+                var yText = Center - (radiusNum - 20) * Math.Sin(-30 * th * (Math.PI / 180) + Math.PI / 2);
+                if (th <= 9)
+                {
+                    g.DrawString(th.ToString(), DefaultFont, new SolidBrush(Color.Black), new Point((int)(xText - 5), (int)(yText - 6)));
+                }
+                else
+                {
+                    g.DrawString(th.ToString(), DefaultFont, new SolidBrush(Color.Black), new Point((int)(xText - 7), (int)(yText - 6)));
+                }
+
+            }
+
+            clockBox.BackgroundImage = back;
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            
+            Refresh();
+            UpdateTime();
+        }
+    }
+}
